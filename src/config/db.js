abdc356d29d5b53af18acdd1c97c8403222da796
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2020-11-28 23:12:53 -0300

    db.js OK

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg')
+
+module.exports = new Pool({
+    user: 'postgres',
+    password: "postgres",
+    host: 'localhost',
+    port: 5433,
+    database: 'smartprint'
+})
